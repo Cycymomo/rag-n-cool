b3a99c354b94477744189ce23d344d5f954381bd
Cyril <cyril.moreau@dailymotion.com>
2018-06-03 16:40:15 +0200

    chore(Routes): setting up the router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route, Link, Switch } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import Home from './pages/Home.jsx';
+import SignUp from './pages/SignUp.jsx';
+import SignIn from './pages/SignIn.jsx';
+import About from './pages/About.jsx';
+import NotFound from './pages/NotFound.jsx';
 import './App.css';
 
+const browserHistory = createBrowserHistory();
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title"> Welcome to React </h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code> src / App.js </code> and save to reload.
-    </p>
-  </div>
+  <Router history={browserHistory}>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Hello world</h1>
+        <Link to="/" href="/">
+          Home
+        </Link>
+        <Link to="/about" href="/about">
+          About
+        </Link>
+        <Link to="/signup" href="/signup">
+          Sign Up
+        </Link>
+        <Link to="/signin" href="/signin">
+          Sign in
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
